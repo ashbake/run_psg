0890e2c739cc28e245d5fe13ee62f70ce5717449
Ashley Baker <ashbake@Ashleys-MBP.T-mobile.com>
2020-10-14 19:31:37 -0700

    add more comments

--- example_run.py
+++ example_run.py
@@ -7,11 +7,12 @@ from tools import *
 
 if __name__=='__main__':
 	# define everything
-	output_path    = './outputs/'
-	obs_time       = '2019/11/18 10:07:16.000'
-	lon, lat, elev = 33.1504, 242.8173, 1.871 # palomar, elevation in km
-	l0, l1, res    = 700, 720, 100000
-	line_list      = 'HIT'
+	output_path    = './outputs/'              # where to dump final spectrum
+	config_path    = './configs/'              # where to dump PSG intermediate config files
+	obs_time       = '2019/11/18 10:07:16.000' # time of observation, must be in this format 
+	lon, lat, elev = 33.1504, 242.8173, 1.871  # palomar lon lat (deg), elevation in km
+	l0, l1, res    = 700, 720, 100000          # wavelength range and resolving power
+	line_list      = 'HIT'                     # 'HIT' or 'GEISA' - must have lines package, HIT more up to date
 
 	# run psg, save telluric spectra to file
 	outfile = run(
@@ -21,10 +22,10 @@ if __name__=='__main__':
 				obs_time,
 				[lon,lat,elev],
 				line_list,
-				output_path=output_path, # path to save final spectrum
+				output_path=output_path,  # path to save final spectrum
 				config_path='./configs/', # path to save intermediate config files
-				plot_path = output_path, # path so save plot
-				mode='fits')              # save as fits
+				plot_path = output_path,  # path so save plot
+				mode='fits')              # save as fits (currently only option)
 
 
 
